127d3d0df095e89ab27a800580f840115f1d61af
Ushkewar Prathmesh Prashant <155670963+Pratham-sggs@users.noreply.github.com>
2026-07-29 00:47:22 +0530

    Time: 192 ms (66.82%) | Memory: 20.7 MB (79.72%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,12 +1,13 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        length = len(s)//2
-        ans = s[:length]
-        ans = sorted(ans)
+        length = len(s) // 2
+        Uni = 97
+        dictionary = defaultdict(int)
+        for i in range(length):
+            dictionary[ord(s[i]) - 97] += 1
         res = ""
-        for i in ans:
-            res = res + i
-        
-        if (len(s)%2):
+        for i in range(0, 26):
+            res = res + dictionary[i]*chr(i+97)
+        if len(s) % 2 :
             return res + s[length] + res[::-1]
-        return res + res[::-1]
+        return res + res[::-1] 
