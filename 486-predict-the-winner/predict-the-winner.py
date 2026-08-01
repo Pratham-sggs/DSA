92e342ac3aa31ee6ca7e8a88c406a73dea51c3ed
Ushkewar Prathmesh Prashant <155670963+Pratham-sggs@users.noreply.github.com>
2026-08-02 00:09:32 +0530

    Time: 3 ms (55.77%) | Memory: 19.2 MB (94.32%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,12 +1,16 @@
 class Solution:
-    def solve(self, i, j, nums):
+    def solve(self, i, j, nums, dp):
         if i > j :
             return 0
-        takeLeft = nums[i] - self.solve(i+1, j, nums)
-        takeRight = nums[j] - self.solve(i, j-1, nums)
-        return max(takeLeft, takeRight)
+        if dp[i][j] != float('inf'):
+            return dp[i][j]
+        takeLeft = nums[i] - self.solve(i+1, j, nums, dp)
+        takeRight = nums[j] - self.solve(i, j-1, nums, dp)
+        dp[i][j] = max(takeLeft, takeRight)
+        return dp[i][j]
 
     def predictTheWinner(self, nums: List[int]) -> bool:
-        res = self.solve(0, len(nums)-1, nums)
+        dp = [[float('inf') for j in range(len(nums))] for i in range(len(nums))]
+        res = self.solve(0, len(nums)-1, nums, dp)
         return True if res >=0 else False
         
