86365f3e0180d10ea03f60994100bc953689c44f
Ushkewar Prathmesh Prashant <155670963+Pratham-sggs@users.noreply.github.com>
2026-08-02 00:04:37 +0530

    Time: 2883 ms (5.67%) | Memory: 19.2 MB (79.49%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,14 +1,12 @@
 class Solution:
-    def solve(self, start, end, person1, person2, chance, dp, nums):
-        if start > end:
-            return person1 >= person2
-        if chance:
-            take = self.solve(start+1, end,  person1+nums[start], person2, False, dp, nums)
-            nottake = self.solve(start, end-1,  person1+nums[end], person2, False, dp, nums)
-            return take or nottake
-        take = self.solve(start+1, end, person1, person2+nums[start], True, dp, nums)
-        nottake = self.solve(start, end-1, person1, person2+nums[end], True, dp, nums)
-        return take and nottake
+    def solve(self, i, j, nums):
+        if i > j :
+            return 0
+        takeLeft = nums[i] - self.solve(i+1, j, nums)
+        takeRight = nums[j] - self.solve(i, j-1, nums)
+        return max(takeLeft, takeRight)
+
     def predictTheWinner(self, nums: List[int]) -> bool:
-        return self.solve(0, len(nums)-1, 0, 0, True, {}, nums)
+        res = self.solve(0, len(nums)-1, nums)
+        return True if res >=0 else False
         
