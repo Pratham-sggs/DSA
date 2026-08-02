b057d534ca86322d0ffa1bb027f220ff0af7072e
Prathmesh Ushkewar <155670963+Pratham-sggs@users.noreply.github.com>
2026-08-03 02:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,9 @@
 class Solution:
-    def solve(self, n, i, dp):
-        if i > n:
-            return 0
-        if i == n:
-            return 1
-        if dp[i] != -1:
-            return dp[i]
-        climb1 = self.solve(n, i+1, dp)
-        climb2 = self.solve(n, i+2, dp)
-        dp[i] = climb1 + climb2
-        return dp[i]
     def climbStairs(self, n: int) -> int:
-        dp = [-1 for _ in range(n)]
-        return self.solve(n, 0, dp)
+        dp = [-1 for _ in range(n+2)]
+        dp[n] = 1
+        dp[n+1] = 0
+        for i in range(n-1, -1, -1):
+            dp[i] = dp[i+1] + dp[i+2]
+        return dp[0]
         
